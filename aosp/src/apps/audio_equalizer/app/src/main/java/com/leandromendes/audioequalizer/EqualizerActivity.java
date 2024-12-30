9bc59ee569bdea28fed9a39dd5a2b173841cd34e
Leandro Mendes dos Santos <leandro.mendes.santtoss@gmail.com>
2024-12-29 23:52:49 -0300

    [DC-2] - Implementation of profile saving
    Signed-off-by: Leandro Mendes dos Santos <leandro.mendes.santtoss@gmail.com>

--- aosp/src/apps/audio_equalizer/app/src/main/java/com/leandromendes/audioequalizer/EqualizerActivity.java
+++ aosp/src/apps/audio_equalizer/app/src/main/java/com/leandromendes/audioequalizer/EqualizerActivity.java
@@ -1,13 +1,20 @@
 package com.leandromendes.audioequalizer;
 
 import android.annotation.SuppressLint;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -19,6 +26,10 @@ public class EqualizerActivity extends AppCompatActivity {
     private TextView textViewBass;
     private TextView textViewMid;
     private TextView textViewTreble;
+    private int bassValue;
+    private int midValue;
+    private int highValue;
+    private int id_position;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -34,7 +45,9 @@ public class EqualizerActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         EqualizerProfile currentProfile = intent.getParcelableExtra("profile");
+        id_position = intent.getIntExtra("position",-1);
 
+        Button saveButton = findViewById(R.id.save);
         textViewProfile = findViewById(R.id.profileSelected);
         textViewBass = findViewById(R.id.textViewBass);
         textViewMid = findViewById(R.id.textViewMid);
@@ -42,22 +55,27 @@ public class EqualizerActivity extends AppCompatActivity {
 
         textViewProfile.setText(currentProfile.getName());
 
-        textViewBass.setText("Bass " + currentProfile.getBassEqValue() + "db");
+        bassValue = currentProfile.getBassEqValue();
+        textViewBass.setText("Bass " + bassValue + "db");
         SeekBar seekBarBass = findViewById(R.id.seekBarBass);
         seekBarBass.setProgress(currentProfile.getBassEqValue());
 
-        textViewMid.setText("Mid " + currentProfile.getMidEqValue() + "db");
+        midValue = currentProfile.getMidEqValue();
+        textViewMid.setText("Mid " + midValue + "db");
         SeekBar seekBarMid = findViewById(R.id.seekBarMid);
         seekBarMid.setProgress(currentProfile.getMidEqValue());
 
-        textViewTreble.setText("Treble " + currentProfile.getHiEqValue() + "db");
+        highValue = currentProfile.getHiEqValue();
+        textViewTreble.setText("Treble " + highValue + "db");
         SeekBar seekBarTreble = findViewById(R.id.seekBarTreble);
         seekBarTreble.setProgress(currentProfile.getHiEqValue());
 
         seekBarBass.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                textViewBass.setText("Bass " + progress + "db");
+                bassValue = progress;
+                textViewBass.setText("Bass " + bassValue + "db");
+                saveButton.setEnabled(true);
             }
 
             @Override
@@ -74,7 +92,9 @@ public class EqualizerActivity extends AppCompatActivity {
         seekBarMid.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                textViewMid.setText("Mid " + progress + "db");
+                midValue = progress;
+                textViewMid.setText("Mid " + midValue + "db");
+                saveButton.setEnabled(true);
             }
 
             @Override
@@ -91,7 +111,9 @@ public class EqualizerActivity extends AppCompatActivity {
         seekBarTreble.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                textViewTreble.setText("Treble " + progress + "db");
+                highValue = progress;
+                textViewTreble.setText("Treble " + highValue + "db");
+                saveButton.setEnabled(true);
             }
 
             @Override
@@ -104,6 +126,23 @@ public class EqualizerActivity extends AppCompatActivity {
 
             }
         });
+
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                    currentProfile.setName("Profile"+(id_position+1));
+                    currentProfile.setBassEqValue(bassValue);
+                    currentProfile.setMidEqValue(midValue);
+                    currentProfile.setHiEqValue(highValue);
+                    Intent resultIntent = new Intent();
+                    resultIntent.putExtra("profile", currentProfile);
+                    resultIntent.putExtra("position", id_position);
+                    setResult(RESULT_OK, resultIntent);
+                    finish();
+                }
+            });
 
     }
 }
--- aosp/src/apps/audio_equalizer/app/src/main/java/com/leandromendes/audioequalizer/MainActivity.java
+++ aosp/src/apps/audio_equalizer/app/src/main/java/com/leandromendes/audioequalizer/MainActivity.java
@@ -1,5 +1,6 @@
 package com.leandromendes.audioequalizer;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,6 +9,10 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResultCallerLauncher;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -48,6 +53,22 @@ public class MainActivity extends AppCompatActivity {
         ProfileList adapter = new ProfileList(this, equalizerProfiles);
         profileList.setAdapter(adapter);
 
+        ActivityResultLauncher<Intent> eqActivity = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if(result.getResultCode() == Activity.RESULT_OK){
+                        Intent intentRet = result.getData();
+                        EqualizerProfile currentProfile = intentRet.getParcelableExtra("profile");
+                        int index = intentRet.getIntExtra("position",-1);;
+                        equalizerProfiles.add(currentProfile);
+                        // Update the ListView adapter
+                        adapter.notifyDataSetChanged();
+                        Toast.makeText(this, "Saved successfully!", Toast.LENGTH_SHORT).show();
+
+                    }
+                }
+        );
+
         // Defines an action for a quick click in the listview
         profileList.setOnItemClickListener((parent, view, position, id) -> {
             EqualizerProfile profileSelected = equalizerProfiles.get(position);
@@ -55,8 +76,10 @@ public class MainActivity extends AppCompatActivity {
 
             Intent intent = new Intent(this, EqualizerActivity.class);
             intent.putExtra("profile", profileSelected);
-            // Start the new Activity
-            startActivity(intent);
+            intent.putExtra("position", position);
+
+            //startActivity(intent);
+            eqActivity.launch(intent);
         });
 
         // Defines an action for a long click on a profile list
@@ -79,6 +102,9 @@ public class MainActivity extends AppCompatActivity {
             return true;
         });
 
+
+
     }
 
 }
+
