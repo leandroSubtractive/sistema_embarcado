42076a513cad6561675d0115b8e10072808d8175
Leandro Mendes dos Santos <leandro.mendes.santtoss@gmail.com>
2025-08-10 15:23:19 -0300

    [dc-5] - Implementation of the first completed practice - Implementation of class VehicleSensorSimulator.kt - Implementation of the visualization screen
    Signed-off-by: Leandro Mendes dos Santos <leandro.mendes.santtoss@gmail.com>

--- aosp/src/apps/VehicleEqualizerApp/app/src/main/java/com/leandromendes/vehicleequalizerapp/MainActivity.kt
+++ aosp/src/apps/VehicleEqualizerApp/app/src/main/java/com/leandromendes/vehicleequalizerapp/MainActivity.kt
@@ -1,20 +1,37 @@
 package com.leandromendes.vehicleequalizerapp
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.leandromendes.vehicleequalizerapp.util.VehicleSensorSimulator
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var speedLabel: TextView // Novo TextView para velocidade
+    private lateinit var readSpeedButton: Button // Novo Botão para velocidade
+
+    private val TAG = "VehicleEqualizerApp"
+
+    // Instância do simulador de sensor de velocidade
+    private val vehicleSensorSimulator = VehicleSensorSimulator("Speed")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // Inicializa o TextView
+        speedLabel = findViewById(R.id.speedLabel)
+        // Inicializa o Botão
+        readSpeedButton = findViewById(R.id.readSpeedButton)
+
+        readSpeedButton.setOnClickListener {
+            val currentSpeed = vehicleSensorSimulator.readSensorData()
+            speedLabel.text = "Velocidade Atual: $currentSpeed km/h"
+            Log.d(TAG, "Velocidade lida: $currentSpeed km/h")
         }
     }
 }
